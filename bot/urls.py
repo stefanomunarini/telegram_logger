a2148a78347630be12901919bdb4d762b5a811db
Stefano <ssteee@gmail.com>
2017-01-28 02:09:27 +0200

    Implement logging to db

--- bot/urls.py
+++ bot/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 
 from bot.views import DispatcherView
-from telegram_logger.credentials.private_keys import BOT_TOKEN
 
 urlpatterns = [
-    url(r'^{}$'.format(BOT_TOKEN), csrf_exempt(DispatcherView.as_view()), name='dispatcher'),
+    url(r'^$', csrf_exempt(DispatcherView.as_view()), name='dispatcher'),
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,46 +1,60 @@
-from queue import Queue
+import json
+import logging
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.views import View
-# import the logging library
-import logging
 
-# Get an instance of a logger
 from bot.models import Chat, User, Message
 
 logger = logging.getLogger(__name__)
 
 
-class DispatcherView(View):
-    def dispatch(self, request, *args, **kwargs):
-        import ipdb; ipdb.set_trace()
-        print('ciao')
-        data = request.body
-        message = data.get('message')
-        chat = message.get('chat')
-        user = message.get('from')
+def set_logging_state(chat_obj, active):
+    chat_obj.logging = active
+    chat_obj.save()
 
-        if message.get('text') == '/stop':
-            logger.error('/stop chat_id={}'.format(chat.get('id')))
-        elif message.get('text') == '/stop':
-            logger.error('/start chat_id={}'.format(chat.get('id')))
+
+class DispatcherView(View):
+    chat = None
+
+    def dispatch(self, request, *args, **kwargs):
+        body_unicode = request.body.decode('utf-8')
+        data = json.loads(body_unicode.rstrip('\n'))
+        message = data.get('message')
+        user = message.get('from')
+        self.chat = message.get('chat')
+
+        chat_obj, _ = Chat.objects.get_or_create(id=self.chat.get('id'))
+        chat_obj.title = self.chat.get('title')
+        chat_obj.type = self.chat.get('type')
+
+        if message['text'] == '/stop':
+            set_logging_state(chat_obj, active=False)
+            return JsonResponse(data=self.create_response(response_message='Logging disabled.'), status=200)
+        elif message['text'] == '/start':
+            set_logging_state(chat_obj, active=True)
+            return JsonResponse(data=self.create_response(response_message='Logging enabled.'), status=200)
         else:
-            user_obj = User.objects.get_or_create(id=user.get('id'))
+            user_obj, _ = User.objects.get_or_create(id=user.get('id'))
             user_obj.first_name = user.get('first_name')
             user_obj.last_name = user.get('last_name')
             user_obj.username = user.get('username')
+            user_obj.save()
 
-            chat_obj = Chat.objects.get_or_create(id=chat.get('id'))
-            chat_obj.title = chat.get('title')
-            chat_obj.type = chat.get('type')
+            if chat_obj.logging:
+                message_obj, _ = Message.objects.get_or_create(message_id=message.get('message_id'),
+                                                               chat=chat_obj,
+                                                               user=user_obj)
+                message_obj.data = datetime.fromtimestamp(message.get('date'))
+                message_obj.text = message.get('text')
+                message_obj.save()
 
-            message_obj = Message.objects.get_or_create(id=message.get('message_id'))
-            message_obj.chat = chat_obj
-            message_obj.user = user_obj
-            message_obj.date = message.get('date')
-            message_obj.text = message.get('text')
+        return JsonResponse(data={}, status=200)
 
-        return JsonResponse(data={'user': user_obj.__dict__,
-                                  'chat': chat_obj.__dict__,
-                                  'message': message_obj.__dict__,
-                                  }, status=200)
+    def create_response(self, response_message):
+        return {
+            'chat_id': self.chat.get('id'),
+            'text': response_message
+        }
+
