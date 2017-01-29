3ccdeed2f676f001263177c82a8b46240fde946e
Stefano <ssteee@gmail.com>
2017-01-29 15:13:41 +0200

    Use search user interface as homepage

--- telegram_logger/urls.py
+++ telegram_logger/urls.py
@@ -23,5 +23,5 @@ from telegram_logger.credentials.private_keys import BOT_TOKEN
 urlpatterns = [
     url(r'^bot/{}'.format(BOT_TOKEN), include(bot_urls, namespace='bot')),
     url(r'^bot/{}/api/v1/'.format(BOT_TOKEN), include(api_urls, namespace='api')),
-    url(r'^search/', include(search_urls, namespace='search')),
+    url(r'', include(search_urls, namespace='search')),
 ]
